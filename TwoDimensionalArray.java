077b789c111edcdf49afe1b2ab68ab014ff07989
monusinchu949 <monusinchu949@gmail.com>
2025-12-08 20:34:44 +0530

    Add files via upload

---
+++ TwoDimensionalArray.java
@@ -0,0 +1,20 @@
+package corejava;
+import java.util.*;
+public class TwoDimensionalArray {
+public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	int arr[][]=new int[2][3];
+	for (int i = 0; i < arr.length; i++) {
+		for (int j = 0; j < arr[i].length; j++) {
+			System.out.println("enter the value for row"+(i+1)+"and column"+(j+1));
+			arr[i][j]=sc.nextInt();
+		}
+	}
+	for (int i = 0; i < arr.length; i++) {
+		for (int j = 0; j < arr[i].length; j++) {
+			System.out.print(arr[i][j]+" ");
+		}
+		System.out.println();
+}
+}
+}
